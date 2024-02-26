df0862cfe14a1326ad4d832f06a90d0d433e970c
Jon Rumsey <jon.rumsey@gmail.com>
2024-02-26 15:47:00 -0800

    Implement, test Factory and Monitoring.

---
+++ BFBMX.Service.Test/Helpers/FileSystemMonitorTests.cs
@@ -0,0 +1,126 @@
+﻿using BFBMX.Service.Helpers;
+
+namespace BFBMX.Service.Test.Helpers
+{
+    public class FileSystemMonitorTests
+    {
+        private readonly string? tempPath;
+        private readonly string tempFilename;
+
+        private string? FwmTempfileFound { get; set; }
+        private string? FwmErrorCallbackMessage { get; set; }
+
+        public FileSystemMonitorTests()
+        {
+            tempPath = Environment.GetEnvironmentVariable("TEMP");
+            tempFilename = "FsmFileTest.mime";
+        }
+
+        [Fact]
+        public void InitializeFileSystemWatcher()
+        {
+            Assert.NotNull(tempPath);
+            var expectedPath = tempPath;
+            var expectedFilter = "*.mime";
+            var expectedIncludeSubdirs = false;
+            var expectedNotifyFilter = NotifyFilters.FileName
+                                     | NotifyFilters.DirectoryName
+                                     | NotifyFilters.CreationTime;
+            var expectedWatcher = new FileSystemWatcher(expectedPath!);
+            expectedWatcher.NotifyFilter = expectedNotifyFilter;
+            expectedWatcher.Filter = expectedFilter;
+            expectedWatcher.IncludeSubdirectories = expectedIncludeSubdirs;
+
+            var actualWatcher = FSWatcherFactory.Create((s, e) => { }, (s, e) => { }, expectedPath!);
+
+            Assert.Equal(expectedPath, actualWatcher.Path);
+            Assert.Equal(expectedFilter, actualWatcher.Filter);
+            Assert.Equal(expectedIncludeSubdirs, actualWatcher.IncludeSubdirectories);
+            Assert.Equal(expectedNotifyFilter, actualWatcher.NotifyFilter);
+            expectedWatcher.Dispose();
+            actualWatcher.Dispose();
+        }
+
+        [Fact]
+        public void StartStopDestroyFileSystemWatcher()
+        {
+            Assert.NotNull(tempPath);
+            var expectedPath = tempPath;
+
+            // capture the watcher object and ensure automatic disposal if a test fails
+            using var actualWatcher =
+                FSWatcherFactory.Create(HandleFileCreated, HandleFileWatherError, expectedPath!);
+
+            // verify starting conditions
+            Assert.Equal(expectedPath, actualWatcher.Path);
+            Assert.True(string.IsNullOrEmpty(FwmTempfileFound));
+            Assert.True(string.IsNullOrEmpty(FwmErrorCallbackMessage));
+
+            // enable the watcher
+            Assert.True(actualWatcher.EnableRaisingEvents = true);
+
+            // create a file to test if the watcher is working
+            var testFile = Path.Combine(tempPath!, tempFilename);
+            string fileContent = "test file content";
+            CancellationToken cancelToken = new CancellationTokenSource().Token;
+
+            var swTask = Task.Run(() =>
+            {
+                using (StreamWriter sw = File.CreateText(testFile))
+                {
+                    sw.WriteLine(fileContent);
+                    Console.WriteLine("Writing file...");
+                }
+            }, cancelToken);
+            swTask.Wait();
+
+            // FwmTempfileFound should be set to the newly created file name
+            Assert.True(File.Exists(testFile));
+            Assert.False(string.IsNullOrWhiteSpace(FwmTempfileFound));
+            Assert.True(string.IsNullOrEmpty(FwmErrorCallbackMessage));
+
+            // reset temp file found
+            FwmTempfileFound = string.Empty;
+            FwmErrorCallbackMessage = string.Empty;
+
+            // turn-off file watching and ensure no further events are raised
+            Assert.False(actualWatcher.EnableRaisingEvents = false);
+
+            // delete the file
+            File.Delete(testFile);
+            Assert.False(File.Exists(testFile));
+
+            // add a test file while the watcher is off
+            var testFile2 = Path.Combine(tempPath!, "FsmFileTest2.mime");
+
+            using (StreamWriter sw = File.CreateText(testFile2))
+            {
+                sw.WriteLine(fileContent);
+            }
+
+            Assert.True(File.Exists(testFile2));
+
+            // test if Created Event was NOT raised
+            Assert.True(string.IsNullOrWhiteSpace(FwmTempfileFound));
+
+            // remove the created file
+            File.Delete(testFile2);
+            // dispose the watcher instance
+            actualWatcher.Dispose();
+        }
+
+#pragma warning disable xUnit1013 // Public method should be marked as test
+        public void HandleFileCreated(object sender, FileSystemEventArgs e)
+#pragma warning restore xUnit1013 // Public method should be marked as test
+        {
+            FwmTempfileFound = e.Name;
+        }
+
+#pragma warning disable xUnit1013 // Public method should be marked as test
+        public void HandleFileWatherError(object sender, ErrorEventArgs e)
+#pragma warning restore xUnit1013 // Public method should be marked as test
+        {
+            FwmErrorCallbackMessage = e.GetException().Message;
+        }
+    }
+}
--- BFBMX.Service/Helpers/FSWatcherFactory.cs
+++ BFBMX.Service/Helpers/FSWatcherFactory.cs
@@ -4,6 +4,13 @@
     {
         private static string DefaultFileType = "*.mime";
 
+        /// <summary>
+        /// Safely generate a new, valid FileSystemWatcher instance.
+        /// </summary>
+        /// <param name="asyncCreatedCallback">A method/delegate handler for when a created file is detected.</param>
+        /// <param name="asyncErrorCallback">A method/delegate handler for when an error occurs.</param>
+        /// <param name="fullpath">The path to watch for files with .mime extension.</param>
+        /// <returns>A new FileSystemWatcher instance.</returns>
         public static FileSystemWatcher Create(FileSystemEventHandler asyncCreatedCallback,
                                                ErrorEventHandler asyncErrorCallback,
                                                string fullpath)
