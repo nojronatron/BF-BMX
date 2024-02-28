19b126304122095a303b682077b907d4687ba91b
Jon Rumsey <jon.rumsey@gmail.com>
2024-02-28 11:47:19 -0800

    Updated models for use with EF.

--- BFBMX.Service/Models/FlaggedBibRecordModel.cs
+++ BFBMX.Service/Models/FlaggedBibRecordModel.cs
@@ -1,15 +1,34 @@
-﻿using System.Text.Json;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace BFBMX.Service.Models
 {
     public class FlaggedBibRecordModel
     {
+        [JsonIgnore]
+        [Key]
+        public int? Id { get; set; }
         public int BibNumber { get; set; } = -1; // max len 15
         public string? @Action { get; set; } = "MISSING"; // IN, OUT, DROP
         public string? BibTimeOfDay { get; set; } // format: HHMM
         public int DayOfMonth { get; set; } = -1; // digit count range(1-31)
         public string? Location { get; set; } = "MISSING"; // max len 26 (as defined by select element in form)
         public bool DataWarning { get; set; } = true;
+
+        // Generate a new instance
+        public static FlaggedBibRecordModel GetBibRecordModel(int bibNumber, string? action, string? bibTimeOfDay, int dayOfMonth, string? location, bool dataWarning)
+        {
+            return new FlaggedBibRecordModel
+            {
+                BibNumber = bibNumber,
+                Action = action,
+                BibTimeOfDay = bibTimeOfDay,
+                DayOfMonth = dayOfMonth,
+                Location = location,
+                DataWarning = dataWarning,
+            };
+        }
 
         public string ToTabbedString()
         {
--- BFBMX.Service/Models/WinlinkMessageModel.cs
+++ BFBMX.Service/Models/WinlinkMessageModel.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Text.Json;
 
@@ -5,14 +6,31 @@ namespace BFBMX.Service.Models;
 
 public class WinlinkMessageModel
 {
+    [Key]
     public string? WinlinkMessageId { get; set; } // max len appears to be 12
     public DateTime MessageDateTime { get; set; } // datetime from Winlink message
     public string? ClientHostname { get; set; } // env:COMPUTERNAME (might not be necessary)
     public List<FlaggedBibRecordModel> BibRecords { get; set; } = new List<FlaggedBibRecordModel>();
 
+    public static WinlinkMessageModel GetWinlinkMessageInstance(string? winlinkMessageId, DateTime messageDateTime, string? clientHostname, List<FlaggedBibRecordModel> bibRecords)
+    {
+        return new WinlinkMessageModel
+        {
+            WinlinkMessageId = winlinkMessageId,
+            MessageDateTime = messageDateTime,
+            ClientHostname = clientHostname,
+            BibRecords = bibRecords,
+        };
+    }
+
     public bool HasDataWarning()
     {
         return BibRecords.Any(x => x.DataWarning);
+    }
+
+    public string PrintableMsgDateTime()
+    {
+        return MessageDateTime.ToString("yyyy-MM-ddTHH-mm-ss");
     }
 
     public string ToFilename()
@@ -20,7 +38,8 @@ public class WinlinkMessageModel
         // learn.microsoft.com: 2009-06-15T13:45:30 (DateTimeKind.Local) -> 2009-06-15T13:45:30
         // string sortableFormatPattern = ClientDateTime.ToString("s");
         string customFormatPattern = MessageDateTime.ToString("yyyy-MM-ddTHH-mm-ss");
-        return $"{ClientHostname}-{customFormatPattern}.txt";
+        //return $"{ClientHostname}-{customFormatPattern}.txt"; // option
+        return $"{WinlinkMessageId}-{customFormatPattern}.txt";
     }
 
     public string ToJsonString()
@@ -29,19 +48,29 @@ public class WinlinkMessageModel
         return JsonSerializer.Serialize<WinlinkMessageModel>(this);
     }
 
-    public override string ToString()
+    public string ToAccessDatabaseTabbedString()
     {
-        // override ToString to get data and collection out of this instance
-        StringBuilder sb = new StringBuilder();
-        sb.Append("Message-ID: ").Append(WinlinkMessageId).Append(" in ");
-        sb.Append(ToFilename()).AppendLine(" contains bib records: [");
+        string recordPrefix = $"{WinlinkMessageId}\t{PrintableMsgDateTime()}\t";
+        StringBuilder sbBibData = new();
 
-        foreach (var record in BibRecords)
+        foreach(var record in BibRecords)
         {
-            sb.AppendLine(record.ToTabbedString());
+            sbBibData.Append(recordPrefix).AppendLine(record.ToTabbedString());
         }
 
-        sb.AppendLine("]");
-        return sb.ToString();
+        return sbBibData.ToString();
+    }
+
+    public string ToServerAuditTabbedString()
+    {
+        string sbHeader = $"{WinlinkMessageId}\t{ClientHostname}\t";
+        StringBuilder sbBibData = new();
+        
+        foreach (var record in BibRecords)
+        {
+            sbBibData.Append(sbHeader).AppendLine(record.ToTabbedString());
+        }
+
+        return sbBibData.ToString();
     }
 }
