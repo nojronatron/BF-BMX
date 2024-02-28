fb21526cc3088a44681147ed78dc76275b82b73c
Jon Rumsey <jon.rumsey@gmail.com>
2024-02-28 11:45:54 -0800

    Server API initial, minor testing done (unsynced).

---
+++ BFBMX.ServerApi/EF/BibMessageContext.cs
@@ -0,0 +1,12 @@
+﻿using BFBMX.Service.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BFBMX.ServerApi.EF;
+
+
+public class BibMessageContext : DbContext
+{
+    public DbSet<WinlinkMessageModel> WinlinkMessageModels { get; set; } = null!;
+    public DbSet<FlaggedBibRecordModel> FlaggedBibRecordModels { get; set; } = null!;
+    public BibMessageContext(DbContextOptions options) : base(options) { }
+}
---
+++ BFBMX.ServerApi/Helpers/HelperReturnModels.cs
@@ -0,0 +1,9 @@
+﻿namespace BFBMX.ServerApi.Helpers
+{
+    public class HelperReturnModels
+    {
+        public string OperationName { get; set; } = string.Empty;
+        public bool OperationSucceeded { get; set; } = false;
+        public string OperationResultMessage { get; set; } = string.Empty;
+    }
+}
