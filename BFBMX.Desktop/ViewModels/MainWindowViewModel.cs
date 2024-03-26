41b30eed8eba0860e2afe000e0ceaac3dc9c36e0
Jon Rumsey <jon.rumsey@gmail.com>
2024-03-25 17:20:00 -0700

    Implement 2nd monitor instance + tweaks.

--- BFBMX.Desktop/ViewModels/MainWindowViewModel.cs
+++ BFBMX.Desktop/ViewModels/MainWindowViewModel.cs
@@ -10,6 +10,10 @@ namespace BFBMX.Desktop.ViewModels
 {
     public partial class MainWindowViewModel : ObservableValidator
     {
+        const string AlphaMonitorName = "Alpha";
+        const string BravoMonitorName = "Bravo";
+        const string CharlieMonitorName = "Charlie";
+
         private readonly ILogger<MainWindowViewModel> _logger;
 
         public MainWindowViewModel(ILogger<MainWindowViewModel> logger)
@@ -20,9 +24,9 @@ namespace BFBMX.Desktop.ViewModels
         [ObservableProperty]
         public string? _alphaStatusMessage;
         [ObservableProperty]
-        public string? _bravoStatusMessage = "Monitor #2 placeholder message.";
+        public string? _bravoStatusMessage;
         [ObservableProperty]
-        public string? _charlieStatusMessage = "Monitor #3 placeholder message.";
+        public string? _charlieStatusMessage = "Monitor #3 is in development.";
 
         [ObservableProperty]
         public DiscoveredFilesCollection? _discoveredFiles = new();
@@ -49,7 +53,6 @@ namespace BFBMX.Desktop.ViewModels
         /***** End Global Monitor Functions *****/
 
         /***** Alpha Monitor Configuration *****/
-        //CancellationTokenSource alphaCancellationToken = new();
         private static FSWMonitor? _alphaMonitor;
 
         [ObservableProperty]
@@ -68,6 +71,9 @@ namespace BFBMX.Desktop.ViewModels
         [NotifyCanExecuteChangedFor(nameof(InitAlphaMonitorCommand), nameof(StartAlphaMonitorCommand), nameof(StopAlphaMonitorCommand), nameof(DestroyAlphaMonitorCommand))]
         public bool _alphaMonitorStarted = false;
 
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(InitAlphaMonitorCommand), nameof(StartAlphaMonitorCommand), nameof(StopAlphaMonitorCommand), nameof(DestroyAlphaMonitorCommand))]
+        public bool _alphaMonitorStopped = true;
 
         /// <summary>
         /// Reset FileSystemWatcher instance to clear monitoring configuration and free memory.
@@ -77,17 +83,49 @@ namespace BFBMX.Desktop.ViewModels
         {
             if (monitor is not null)
             {
-                monitor.EnableRaisingEvents = false;
-                monitor.Dispose();
-                //AlphaStatusMessage = "Rest Monitor called and Monitor has been destroyed.";
-                _logger.LogInformation("ResetMonitor: An existing Alpha Monitor was disposed.");
-                AlphaMonitorStarted = false;
-                AlphaMonitorInitialized = false;
+
+                switch (monitor.GetName())
+                {
+                    case AlphaMonitorName:
+                        {
+                            monitor.EnableRaisingEvents = false;
+                            monitor.Dispose();
+                            _logger.LogInformation("ResetMonitor: An existing Alpha Monitor was disposed.");
+                            AlphaMonitorPathEnabled = true;
+                            AlphaMonitorInitialized = false;
+                            AlphaStatusMessage = "This monitor has been reset.";
+                            break;
+                        }
+                    case BravoMonitorName:
+                        {
+                            monitor.EnableRaisingEvents = false;
+                            monitor.Dispose();
+                            _logger.LogInformation("ResetMonitor: An existing Bravo Monitor was disposed.");
+                            BravoMonitorPathEnabled = true;
+                            BravoMonitorInitialized = false;
+                            BravoStatusMessage = "This monitor has been reset.";
+                            break;
+                        }
+                    case CharlieMonitorName:
+                        {
+                            //monitor.EnableRaisingEvents = false;
+                            //monitor.Dispose();
+                            //_logger.LogInformation("ResetMonitor: An existing Charlie Monitor was disposed.");
+                            //CharlieMonitorPathEnabled = true;
+                            //CharlieMonitorInitialized = false;
+                            //AlphaStatusMessage = "This monitor has been reset.";
+                            break;
+                        }
+                    default:
+                        {
+                            _logger.LogWarning("ResetMonitor: Monitor name not recognized: {monitorName}", monitor.GetName());
+                            break;
+                        }
+                }
             }
             else
             {
-                //AlphaStatusMessage = "Reset Monitor called but Monitor is not initialized or started.";
-                _logger.LogInformation("ResetMonitor: No existing Alpha Monitor to dispose, nothing to do!");
+                _logger.LogInformation("ResetMonitor: No existing Monitor to dispose. All monitors states unchanged.");
             }
         }
 
@@ -104,7 +142,7 @@ namespace BFBMX.Desktop.ViewModels
 
             try
             {
-                _alphaMonitor = FSWatcherFactory.Create(HandleFileCreatedAsync, HandleError, AlphaMonitorPath!);
+                _alphaMonitor = FSWatcherFactory.Create(HandleFileCreatedAsync, HandleError, AlphaMonitorPath!, AlphaMonitorName);
                 AlphaMonitorPathEnabled = false;
                 AlphaMonitorInitialized = _alphaMonitor!.IsInitialized;
                 string isOrNotInitialized = AlphaMonitorInitialized ? "successfully" : "not";
@@ -124,13 +162,11 @@ namespace BFBMX.Desktop.ViewModels
         {
             if (string.IsNullOrWhiteSpace(AlphaMonitorPath))
             {
-                //AlphaStatusMessage = "Enter a valid path to monitor before initializing.";
                 _logger.LogInformation("CanInitAlphaMonitor: Alpha Monitor path is null or empty, cannot initialize.");
                 return false;
             }
             else
             {
-                //AlphaStatusMessage = "Monitor initialized.";
                 _logger.LogInformation("CanInitAlphaMonitor: Returning true for path {alphaMonPath}", AlphaMonitorPath);
                 return true;
             }
@@ -190,7 +226,7 @@ namespace BFBMX.Desktop.ViewModels
                 AlphaMonitorStarted = _alphaMonitor!.IsStarted;
                 string stopOrNot = AlphaMonitorStarted ? "not" : "successfully";
                 AlphaStatusMessage = "Monitor stopped successfully.";
-                _logger.LogInformation("StartAlphaMonitor: Monitor {stopOrNot} started for path {monitorPath}", stopOrNot, AlphaMonitorPath);
+                _logger.LogInformation("StopAlphaMonitor: Monitor {stopOrNot} stopped for path {monitorPath}", stopOrNot, AlphaMonitorPath);
             }
             catch (Exception ex)
             {
@@ -257,56 +293,186 @@ namespace BFBMX.Desktop.ViewModels
             }
 
             return true;
-
         }
+
         /***** End Alpha Monitor Configuration *****/
 
         // todo: consider how to multiplex the monitor configuration and commands for all 3 monitors.
 
         /***** Bravo Monitor Configuration *****/
+        private static FSWMonitor? _bravoMonitor;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(InitBravoMonitorCommand), nameof(DestroyBravoMonitorCommand))]
+        public bool _bravoMonitorPathEnabled = true;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(InitBravoMonitorCommand), nameof(StartBravoMonitorCommand), nameof(StopBravoMonitorCommand), nameof(DestroyBravoMonitorCommand))]
+        public string? _bravoMonitorPath = string.Empty;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(InitBravoMonitorCommand), nameof(StartBravoMonitorCommand), nameof(StopBravoMonitorCommand), nameof(DestroyBravoMonitorCommand))]
+        public bool _bravoMonitorInitialized = false;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(InitBravoMonitorCommand), nameof(StartBravoMonitorCommand), nameof(StopBravoMonitorCommand), nameof(DestroyBravoMonitorCommand))]
+        public bool _bravoMonitorStarted = false;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(InitBravoMonitorCommand), nameof(StartBravoMonitorCommand), nameof(StopBravoMonitorCommand), nameof(DestroyBravoMonitorCommand))]
+        public bool _bravoMonitorStopped = true;
+
         [RelayCommand(CanExecute = nameof(CanInitBravoMonitor))]
         public void InitBravoMonitor()
         {
+            _logger.LogInformation("InitBravoMonitor button pressed.");
 
+            if (_bravoMonitor is not null)
+            {
+                _logger.LogInformation("InitBravoMonitor: Resetting existing Bravo Monitor.");
+                ResetMonitor(_bravoMonitor);
+            }
+
+            try
+            {
+                _bravoMonitor = FSWatcherFactory.Create(HandleFileCreatedAsync, HandleError, BravoMonitorPath!, BravoMonitorName);
+                BravoMonitorPathEnabled = false;
+                BravoMonitorInitialized = _bravoMonitor!.IsInitialized;
+                string isOrNotInitialized = BravoMonitorInitialized ? "successfully" : "not";
+                BravoStatusMessage = "Monitor initialized.";
+                _logger.LogInformation("Bravo Monitor {isOrNotInit} initialized for path: {monitorPath}", isOrNotInitialized, BravoMonitorPath);
+            }
+            catch (Exception ex)
+            {
+                BravoStatusMessage = "Unable to initialize! Try the Destroy button, then input the path again.";
+                _logger.LogInformation("Bravo Monitor unable to initialize for {monitorPath}, exception msg: {exceptionMsg}",
+                                      BravoMonitorPath, ex.Message);
+                BravoMonitorPathEnabled = true;
+            }
         }
 
         public bool CanInitBravoMonitor()
         {
-            return false;
+            if (string.IsNullOrWhiteSpace(BravoMonitorPath))
+            {
+                _logger.LogInformation("CanInitBravoMonitor: Bravo Monitor path is null or empty, cannot initialize.");
+                return false;
+            }
+            else
+            {
+                _logger.LogInformation("CanInitBravoMonitor: Returning true for path {BravoMonPath}", BravoMonitorPath);
+                return true;
+            }
         }
 
         [RelayCommand(CanExecute = nameof(CanStartBravoMonitor))]
         public void StartBravoMonitor()
         {
-
+            if (_bravoMonitor is not null && _bravoMonitor.CanStart())
+            {
+                _bravoMonitor.EnableRaisingEvents = true;
+                BravoMonitorStarted = _bravoMonitor.IsStarted;
+                string isOrNot = _bravoMonitor.IsStarted ? "successfully" : "not";
+                BravoStatusMessage = $"Monitor {isOrNot} started for path.";
+                _logger.LogInformation("StartBravoMonitor: Monitor {isOrNot} started for path {monitorPath}", isOrNot, BravoMonitorPath);
+            }
+            else
+            {
+                BravoMonitorStarted = false;
+                BravoStatusMessage = "Unable to start monitor! Check logs then try Destroy or Initialize instead.";
+                _logger.LogWarning("StartBravoMonitor: Unable to enable raising events for {monitorpath}!", BravoMonitorPath);
+            }
         }
 
         public bool CanStartBravoMonitor()
         {
+            if (_bravoMonitor is not null && _bravoMonitor.CanStart())
+            {
+                _logger.LogInformation("CanStartBravoMonitor: BravoMonitor is not null, is not enabled to raise events, and path matches. Returning true.");
+                return true;
+            }
+            else
+            {
+                bool isNull = _bravoMonitor is null;
+                bool isEnabled = _bravoMonitor?.EnableRaisingEvents == true;
+                bool pathMatches = _bravoMonitor?.MonitoredPath == BravoMonitorPath;
+                _logger.LogWarning("CanStartBravoMonitor: Monitor is null? {isnull}; isEnabled? {isenabled}; pathMatches? {pathmatches}.",
+                    isNull, isEnabled, pathMatches);
+            }
+
             return false;
         }
 
         [RelayCommand(CanExecute = nameof(CanStopBravoMonitor))]
         public void StopBravoMonitor()
         {
-
+            if (_bravoMonitor is not null && _bravoMonitor.IsStarted)
+            {
+                _bravoMonitor.EnableRaisingEvents = false;
+                BravoMonitorStarted = _bravoMonitor.IsStarted;
+                string isOrNot = _bravoMonitor.IsStarted ? "not" : "successfully";
+                BravoStatusMessage = "Monitor stopped successfully.";
+                _logger.LogInformation("StopBravoMonitor: Monitor {isOrNot} stopped for path {monitorPath}", isOrNot, AlphaMonitorPath);
+            }
+            else
+            {
+                BravoMonitorStarted = _bravoMonitor?.IsStarted ?? false;
+                BravoStatusMessage = "Unable to stop monitor! Check logs then try Destroy or Initialize instead.";
+                _logger.LogWarning("StopBravoMonitor: Unable to disable raising events for {monitorpath}!", BravoMonitorPath);
+            }
         }
 
         public bool CanStopBravoMonitor()
         {
-            return true;
+            if (_bravoMonitor is not null && _bravoMonitor.IsStarted)
+            {
+                _logger.LogInformation("CanStopBravoMonitor: BravoMonitor is not null, is enabled to raise events, and path matches. Returning true.");
+                return true;
+            }
+            else
+            {
+                bool isNull = _bravoMonitor is null;
+                bool isEnabled = _bravoMonitor?.EnableRaisingEvents == true;
+                bool pathMatches = _bravoMonitor?.MonitoredPath == BravoMonitorPath;
+                _logger.LogWarning("CanStopBravoMonitor: Monitor is null? {isnull}; isEnabled? {isenabled}; pathMatches? {pathmatches}.",
+                                       isNull, isEnabled, pathMatches);
+            }
+
+            return false;
         }
 
         [RelayCommand(CanExecute = nameof(CanDestroyBravoMonitor))]
         public void DestroyBravoMonitor()
         {
-
+            _logger.LogInformation("DestroyBravoMonitor: Button pressed.");
+            ResetMonitor(_bravoMonitor);
+            BravoStatusMessage = "Monitor has been reset.";
+            BravoMonitorPathEnabled = true;
         }
 
         public bool CanDestroyBravoMonitor()
         {
+            if (_bravoMonitor is null)
+            {
+                _logger.LogInformation("CanDestroyBravoMonitor: Bravo Monitor already destroyed. Try to initialize new, instead.");
+                return false;
+            }
+
+            if (_bravoMonitor.MonitoredPath != BravoMonitorPath)
+            {
+                _logger.LogInformation("CanDestroyBravoMonitor: Monitor Path {monActualPath} neq {monConfiguredPath} but will return true anyway.",
+                                      _bravoMonitor.MonitoredPath,
+                                      BravoMonitorPath);
+            }
+
+            if (_bravoMonitor.EnableRaisingEvents == false)
+            {
+                _logger.LogInformation("CanDestroyBravoMonitor: Monitor is not raising events but will return true anyway.");
+            }
+
             return true;
         }
+
         /***** End Bravo Monitor Commands *****/
 
         /***** Charlie Monitor Commands *****/
