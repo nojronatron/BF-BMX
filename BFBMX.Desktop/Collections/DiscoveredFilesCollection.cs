5f924df866985d031b8640a374c86e7ad50abdf1
Jon Rumsey <jon.rumsey@gmail.com>
2024-04-01 16:41:42 -0700

    Warn Log dupe filenames. Fixed async operations.

--- BFBMX.Desktop/Collections/DiscoveredFilesCollection.cs
+++ BFBMX.Desktop/Collections/DiscoveredFilesCollection.cs
@@ -21,30 +21,39 @@ namespace BFBMX.Desktop.Collections
     {
         private readonly IApiClient _apiClient;
         private readonly ILogger<DiscoveredFilesCollection> _logger;
+        private readonly IFileProcessor _fileProcessor;
 
         public int MaxItems { get; } = 6;
 
-        public DiscoveredFilesCollection(ILogger<DiscoveredFilesCollection> logger,
-            IApiClient apiClient)
+        public DiscoveredFilesCollection(
+            ILogger<DiscoveredFilesCollection> logger,
+            IApiClient apiClient,
+            IFileProcessor fileProcessor)
         {
             _apiClient = apiClient;
             _logger = logger;
+            _fileProcessor = fileProcessor;
         }
 
         public async Task EnqueueAsync(DiscoveredFileModel item)
         {
+            if( this.Any(x => x.FullFilePath == item.FullFilePath))
+            {
+                _logger.LogWarning("DiscoveredFilesCollection EnqueueAsync: File {filepath} is a DUPLICATE. Storing in memory but contents might not get added to database.", item.FullFilePath);
+            }
+
             Enqueue(item);
-            WinlinkMessageModel winlinkMessage = FileProcessor.ProcessWinlinkMessageFile(item.FileTimeStamp, Environment.MachineName, item.FullFilePath);
+            WinlinkMessageModel winlinkMessage = _fileProcessor.ProcessWinlinkMessageFile(item.FileTimeStamp, Environment.MachineName, item.FullFilePath);
+
             if (winlinkMessage is not null && winlinkMessage.BibRecords.Count > 0)
             {
-                // todo: address these hanging, unused, promised variables
-                bool wroteToFile = await FileProcessor.WriteWinlinkMessageToFile(winlinkMessage, Path.Combine(DesktopEnvFactory.GetBfBmxLogPath(), DesktopEnvFactory.GetBibRecordsLogFileName()));
+                bool wroteToFile = _fileProcessor.WriteWinlinkMessageToFile(winlinkMessage, Path.Combine(DesktopEnvFactory.GetBfBmxLogPath(), DesktopEnvFactory.GetBibRecordsLogFileName()));
                 bool postedToApi = await _apiClient.PostWinlinkMessageAsync(winlinkMessage.ToJsonString());
-                _logger.LogInformation("DiscoveredFilesCollection: EnqueueAync: MaxItems: {maxitems}. Item count: {count}.", MaxItems, Count);
+                _logger.LogInformation("DiscoveredFilesCollection EnqueueAync: Wrote to file? {wroteToFile}. Posted to API? {postedToApi}. Items in collection: {collectionCount}.", wroteToFile, postedToApi, Count);
             }
             else
             {
-                _logger.LogInformation("DiscoveredFilesCollection: EnqueueAsync: No bibrecords found in winlinkMessage.");
+                _logger.LogInformation("DiscoveredFilesCollection EnqueueAsync: No bibrecords found in winlinkMessage.");
             }
         }
     }
