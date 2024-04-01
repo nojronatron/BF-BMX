bc77bd3c63d62fb811826ea1031ea475e656aaf2
Jon Rumsey <jon.rumsey@gmail.com>
2024-04-01 16:49:45 -0700

    Implement server env factory.

---
+++ BFBMX.ServerApi/Helpers/ServerEnvFactory.cs
@@ -0,0 +1,34 @@
+﻿namespace BFBMX.ServerApi.Helpers
+{
+    public static class ServerEnvFactory
+    {
+        public static string GetuserProfilePath()
+        {
+            string? userProfilePath = Environment.GetEnvironmentVariable("USERPROFILE");
+            string upPath = string.IsNullOrWhiteSpace(userProfilePath) ? @"C:\" : userProfilePath;
+            return upPath;
+        }
+
+        public static string GetServerFolderName()
+        {
+            string? logDirectory = Environment.GetEnvironmentVariable("BFBMX_SERVER_FOLDER_NAME");
+            string sfName = string.IsNullOrWhiteSpace(logDirectory) ? "BFBMX" : logDirectory;
+            return sfName;
+        }
+
+        public static string GetServerBackupFilename()
+        {
+            string? bbBackupFilename = Environment.GetEnvironmentVariable("BFBMX_BACKUP_FILE_NAME");
+            string backupFileName = string.IsNullOrWhiteSpace(bbBackupFilename) ? "BFBMX-LocalDb-Backup.txt" : bbBackupFilename;
+            return backupFileName;
+        }
+
+        public static string GetServerLogPath()
+        {
+            string userProfilePath = GetuserProfilePath();
+            string logDirectory = GetServerFolderName();
+            string serverLogPath = System.IO.Path.Combine(userProfilePath, "Documents", logDirectory);
+            return serverLogPath;
+        }
+    }
+}
