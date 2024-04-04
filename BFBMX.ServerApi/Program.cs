c4e4c65fa99220e7cfaeb2c792e7a287c7446e11
Jon Rumsey <jon.rumsey@gmail.com>
2024-04-04 15:38:14 -0700

    Fix PORT assignment.

--- BFBMX.ServerApi/Program.cs
+++ BFBMX.ServerApi/Program.cs
@@ -7,7 +7,7 @@ using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
-string serverPort = Environment.GetEnvironmentVariable("BFBMX_SERVER_PORT") ?? "5000";
+string serverPort = Environment.GetEnvironmentVariable("BFBMX_SERVERPORT") ?? "5150";
 int.TryParse(serverPort, out int srvrPort);
 
 // Add services to the container.
