6498ca3c84425a299c4d795aa174424562b61e53
Jon Rumsey <jon.rumsey@gmail.com>
2024-05-01 16:58:42 -0700

    Add tests for question mark detection cases.

--- BFBMX.Service.Test/Helpers/FileProcessorTests.cs
+++ BFBMX.Service.Test/Helpers/FileProcessorTests.cs
@@ -1,7 +1,6 @@
 using BFBMX.Service.Helpers;
 using BFBMX.Service.Models;
 using BFBMX.Service.Test.TestData;
-using System.Diagnostics;
 using Moq;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.ObjectPool;
@@ -27,6 +26,118 @@ public class FileProcessorTests
         _fileProcessor = new FileProcessor(_mockLogger.Object);
     }
 
+    [Theory]
+    [InlineData(new string[] { "84\tOUT\t1234\t15\t??" }, 0)]
+    [InlineData(new string[] { "84,OUT,1234,15,??" }, 0)]
+    public void NoLocationNoFindBibRecords(string[] lines, int expectedCount)
+    {
+        List<FlaggedBibRecordModel> actualResult = _fileProcessor.GetStrictMatches(lines).ToList();
+        actualResult.AddRange(_fileProcessor.GetSloppyMatches(lines));
+        Assert.Equal(expectedCount, actualResult.Count);
+    }
+
+    [Theory]
+    [InlineData(new string[] { "???\tOUT\t1234\t15\tQR" }, 0)]
+    [InlineData(new string[] { "???,OUT,1234,15,QR" }, 0)]
+    public void NoBibNumberNoFindBibRecordsPeriod(string[] lines, int expectedCount)
+    {
+        List<FlaggedBibRecordModel> actualResult = _fileProcessor.GetStrictMatches(lines).ToList();
+        actualResult.AddRange(_fileProcessor.GetSloppyMatches(lines));
+        Assert.Equal(expectedCount, actualResult.Count);
+    }
+
+    [Theory]
+    [InlineData(new string[] { "1 OUT 1 1 AB" }, 0)]
+    [InlineData(new string[] { "1 IN 1 1 AB" }, 0)]
+    [InlineData(new string[] { "1 IN 1 1 AB", "1 OUT 1 1 AB" }, 0)]
+    [InlineData(new string[] { "1 IN 1 1 AB", "1 IN 1 1 AB" }, 0)]
+    public void StrictMatcherCannotFindSpaceDelimitedBibRecords(string[] lines, int expectedCount)
+    {
+        List<FlaggedBibRecordModel> actualResult = _fileProcessor.GetStrictMatches(lines).ToList();
+        Assert.NotNull(actualResult);
+        Assert.Equal(expectedCount, actualResult.Count);
+    }
+
+    [Theory]
+    [InlineData(new string[] { "1 OUT 1 1 AB" }, 0)]
+    [InlineData(new string[] { "1 IN 1 1 AB" }, 0)]
+    [InlineData(new string[] { "1 IN 1 1 AB", "1 OUT 1 1 AB" }, 0)]
+    [InlineData(new string[] { "1 IN 1 1 AB", "1 IN 1 1 AB" }, 0)]
+    public void SloppyMatcherCannotFindSpaceDelimitedBibRecords(string[] inputData, int expectedCount)
+    {
+        HashSet<FlaggedBibRecordModel> sloppyMatches = _fileProcessor.GetSloppyMatches(inputData);
+        int actualCount = sloppyMatches.Count;
+        Assert.Equal(expectedCount, actualCount);
+    }
+
+    [Theory]
+    [InlineData("84\tOUT\t???\t15\tQR", 1)]
+    [InlineData("84\t???\t1234\t15\tQR", 1)]
+    [InlineData("84\tOUT\t1234\t???\tQR", 1)]
+    [InlineData("84\t???\t???\t15\tQR", 1)]
+    [InlineData("84,OUT,???,15,QR", 1)]
+    [InlineData("84,???,1234,15,QR", 1)]
+    [InlineData("84,OUT,1234,???,QR", 1)]
+    [InlineData("84,???,???,15,QR", 1)]
+    public void SloppyMatcherFindsBibRecordsWithQueryMarks(string inputData, int expectedCount)
+    {
+        string[] inputArr = new string[] { inputData };
+        HashSet<FlaggedBibRecordModel> sloppyMatches = _fileProcessor.GetSloppyMatches(inputArr);
+        int actualCount = sloppyMatches.Count;
+        Assert.Equal(expectedCount, actualCount);
+    }
+
+    [Theory]
+    [InlineData("84 OUT 2346 14 SB", 0)]
+    [InlineData("Cc: N0CAL; N0CAl=20", 0)]
+    [InlineData("Cc: N0CAl=20", 0)]
+    [InlineData("Source: WB7AAV", 0)]
+    [InlineData("Date: Sat, 12 Aug 2023 09:33:00 +0000", 0)]
+    [InlineData("From: N0ONE@winlink.org", 0)]
+    [InlineData("Subject: FW: Bigfoot 200 Coldwater Lake Message $7", 0)]
+    [InlineData("To: N0CAL@winlink.org", 0)]
+    [InlineData("Message-ID: U3BW9J0QHAPR", 0)]
+    [InlineData("X-Source: N0CAL", 0)]
+    [InlineData("X-P2P: True", 0)]
+    [InlineData("X-Location: 46.520833N, 121.958333W (Grid square)", 0)]
+    [InlineData("MIME-Version: 1.0", 0)]
+    [InlineData("Content-Type: multipart/mixed; boundary=\"boundaryfdj01Q==\"", 0)]
+    [InlineData("--boundaryfdj01Q==", 0)]
+    [InlineData("Content-Type: text/plain; charset=\"iso-8859-1\"", 0)]
+    [InlineData("Content-Transfer-Encoding; quoted-printable", 0)]
+    [InlineData("\n", 0)]
+    [InlineData("\r\n", 0)]
+    [InlineData("\r", 0)]
+    [InlineData("\n\r", 0)]
+    [InlineData("----- Message from N0CAL was forwarded by N0CAL at 2023-08-12 09:33 UTC ---", 0)]
+    [InlineData("--", 0)]
+    [InlineData("Message ID: U3BW9J0QHAPR", 0)]
+    [InlineData("Date: 2022/08/16 00:30", 0)]
+    [InlineData("From: N0ONE", 0)]
+    [InlineData("To: N0ONE; C4LL=20", 0)]
+    [InlineData("To: N0ONE=20", 0)]
+    [InlineData("To: N0ONE", 0)]
+    [InlineData("Source: N0ONE", 0)]
+    [InlineData("Location: 46.437500N, 121.625000W (GRID SQUARE)", 0)]
+    [InlineData("Subject: Bigfoot 200 Coldwater Lake Message #7", 0)]
+    [InlineData("-", 0)]
+    [InlineData("-----", 0)]
+    [InlineData("", 0)]
+    [InlineData("=20", 0)]
+    [InlineData("some out times are unknown", 0)]
+    [InlineData("* The entries in this email are TAB delimited. This allows you to copy and =\n", 0)]
+    [InlineData("paste into a spreadsheet.\n", 0)]
+    [InlineData("Winlink Express Sender N0CAL=20", 0)]
+    [InlineData("Template version 1.1.1", 0)]
+    [InlineData("--boundaryfdjo1Q==--", 0)]
+    public void SloppyMatcherCannotFindOtherMessageInformationWithSpaces(string inputData, int expectedCount)
+    {
+        string[] inputArr = new string[] { inputData };
+        HashSet<FlaggedBibRecordModel> sloppyMatches = _fileProcessor.GetSloppyMatches(inputArr);
+        int actualCount = sloppyMatches.Count;
+        Assert.Equal(expectedCount, actualCount);
+    }
+
     [Fact]
     public void FileDoesNotExist_ReturnsZeroCountList()
     {
@@ -36,43 +147,44 @@ public class FileProcessorTests
         Assert.Equal(expectedCount, actualCount);
     }
 
-    [Fact]
-    public void FileDoesExist_ReturnsPositiveCountList()
-    {
-        if (!string.IsNullOrWhiteSpace(GetTempDirectory))
-        {
-            var tempFile = Path.Combine(GetTempDirectory, "tempfiledata.txt");
-            string tempfileData = "This is a test file.\nIt has four lines.\nIt is a test.\nIt is only a test.";
+    // Do not delete will be reimplemented after refactoring FileProcessor
+    //[Fact]
+    //public void FileDoesExist_ReturnsPositiveCountList()
+    //{
+    //    if (!string.IsNullOrWhiteSpace(GetTempDirectory))
+    //    {
+    //        var tempFile = Path.Combine(GetTempDirectory, "tempfiledata.txt");
+    //        string tempfileData = "This is a test file.\nIt has four lines.\nIt is a test.\nIt is only a test.";
 
-            if (File.Exists(tempFile))
-            {
-                File.Delete(tempFile);
-            }
-            else
-            {
-                File.WriteAllLines(tempFile, tempfileData.Split('\n'));
-            }
+    //        if (File.Exists(tempFile))
+    //        {
+    //            File.Delete(tempFile);
+    //        }
+    //        else
+    //        {
+    //            File.WriteAllLines(tempFile, tempfileData.Split('\n'));
+    //        }
 
-            var expectedCount = 4;
-            var actualResult = _fileProcessor.GetFileData(tempFile);
-            var actualCount = actualResult.Length;
-            Assert.Equal(expectedCount, actualCount);
+    //        var expectedCount = 4;
+    //        var actualResult = _fileProcessor.GetFileData(tempFile);
+    //        var actualCount = actualResult.Length;
+    //        Assert.Equal(expectedCount, actualCount);
 
-            if (File.Exists(tempFile))
-            {
-                File.Delete(tempFile);
-            }
-        }
-        else
-        {
-            Assert.True(false, "Environment variable TEMP not found.");
-        }
-    }
+    //        if (File.Exists(tempFile))
+    //        {
+    //            File.Delete(tempFile);
+    //        }
+    //    }
+    //    else
+    //    {
+    //        Assert.True(false, "Environment variable TEMP not found.");
+    //    }
+    //}
 
+    [Theory]
     [InlineData(new string[] { "115\tOUT\t2009\t11\tWR", "195\tOUT\t2009\t11\tWR", "196\tOUT\t2009\t11\tWR" }, 3)]
     [InlineData(new string[] { "236\tOUT\t2231\t14\tSB", "13\tIN\t2231\t14\tSB", "118\tIN\t2235\t14\tSB" }, 3)]
     [InlineData(new string[] { "84\tIN\t2346\t14\tSB", "31\tIN\t2348\t14\tSB", "174\tDROP\t2355\t14\tSB" }, 3)]
-    [Theory]
     public void CapturesThreeTabbedRecords_WellFormattedStrictData(string[] bibArray, int expectedCount)
     {
         List<FlaggedBibRecordModel> actualResult = _fileProcessor.GetStrictMatches(bibArray).ToList();
@@ -80,9 +192,9 @@ public class FileProcessorTests
         Assert.Equal(expectedCount, actualResult.Count);
     }
 
+    [Theory]
     [InlineData(new string[] { "217\tOUT\tunknow\t11\tCW", "307362\tOUT\t0948\t11\tBL", "198\tOUT\t18009\t12\tEPEP" }, 3)]
     [InlineData(new string[] { @"115	0UT	2oo9	II	WR", @"195	OUT	2009	11	WR", @"196	OUT	2009	11	WR" }, 3)]
-    [Theory]
     public void CapturesThreeTabRecords_WellFormattedSloppyData(string[] tabbedBibData, int expectedCount)
     {
         List<FlaggedBibRecordModel> actualTabDelimitedResult = _fileProcessor.GetSloppyMatches(tabbedBibData).ToList();
@@ -118,9 +230,9 @@ public class FileProcessorTests
         Assert.Equal(expectedCount, bibRecords.Count);
     }
 
+    [Theory]
     [InlineData(new string[] { "206 ,IN ,0311 ,27 ,TS", "60 ,IN ,0301 ,27 ,TS" }, 2)]
     [InlineData(new string[] { "206 , IN , 0311 , 27 , TS", "60 , IN , 0301 , 27 , TS" }, 2)]
-    [Theory]
     public void CapturesCorrectNumber_PoorlyFormattedCommaSeparatedValidBibRecords(string[] commaBibRecords, int expectedCount)
     {  
         List<FlaggedBibRecordModel> actualCommaDelmitedResult = _fileProcessor.GetStrictMatches(commaBibRecords).ToList();
@@ -128,9 +240,9 @@ public class FileProcessorTests
         Assert.Equal(expectedCount, actualCommaDelmitedResult.Count);
     }
 
+    [Theory]
     [InlineData(new string[] { "206,IN,0311,27,TS", "60,IN,0301,27,TS" }, 2)]
     [InlineData(new string[] { "206, IN, 0311, 27, TS", "60, IN, 0301, 27, TS" }, 2)]
-    [Theory]
     public void CapturesCorrectNumber_WellFormattedCommaSeparatedValidBibRecords(string[] commaBibRecords, int expectedCount)
     {
         List<FlaggedBibRecordModel> actualCommaDelmitedResult = _fileProcessor.GetStrictMatches(commaBibRecords).ToList();
@@ -138,12 +250,12 @@ public class FileProcessorTests
         Assert.Equal(expectedCount, actualCommaDelmitedResult.Count);
     }
 
+    [Theory]
     [InlineData(new string[] {
           "115 , 0UT,2oo9, II ,WR",
           "19S,OUT,2009,11,WR",
           "196, OUTIE ,2009 , 11,  Wright Meadow"
         }, 3)]
-    [Theory]
     public void CapturesThreeCommaRecords_PoorFormattingAndMistypedValues(string[] commaBibRecords, int expectedCount)
     {
         List<FlaggedBibRecordModel> actualCommaDelimitedResult = _fileProcessor.GetSloppyMatches(commaBibRecords).ToList();
@@ -246,17 +358,6 @@ public class FileProcessorTests
         Assert.Equal(expectedCountDelta, actualSloppyResultListDelta.Count);
     }
 
-    [InlineData(new string[] { "1 OUT 1 1 AB" }, 0)]
-    [InlineData(new string[] { "1 IN 1 1 AB" }, 0)]
-    [InlineData(new string[] { "1 IN 1 1 AB", "1 OUT 1 1 AB" }, 0)]
-    [InlineData(new string[] { "1 IN 1 1 AB", "1 IN 1 1 AB" }, 0)]
-    [Theory]
-    public void UnableToMatch_SpaceDelimitedBibRecords(string[] lines, int expectedCount)
-    {
-        List<FlaggedBibRecordModel> actualResult = _fileProcessor.GetStrictMatches(lines).ToList();
-        Assert.NotNull(actualResult);
-        Assert.Equal(expectedCount, actualResult.Count);
-    }
 
     [Fact]
     public void MatchersFailToMatchSpaceDelimitedBibsInValidMessage()
@@ -289,75 +390,77 @@ public class FileProcessorTests
         Assert.Equal(expectedLooseCount, actualSloppyResultList.Count);
     }
 
-    [Fact]
-    public void WriteWinlinkMessageToFile_WritesFileSuccessfully()
-    {
-        // Arrange
-        var targetFolder = Environment.SpecialFolder.CommonDocuments;
-        string fileName = "testFilePath";
-        string filePath = Path.Combine(Environment.GetFolderPath(targetFolder), fileName);
-        Console.WriteLine($"Target filepath is: {filePath}");
+    // DO NOT REMOVE will be updated with FileProcessor is refactored.
+    //[Fact]
+    //public void WriteWinlinkMessageToFile_WritesFileSuccessfully()
+    //{
+    //    // Arrange
+    //    var targetFolder = Environment.SpecialFolder.CommonDocuments;
+    //    string fileName = "testFilePath";
+    //    string filePath = Path.Combine(Environment.GetFolderPath(targetFolder), fileName);
+    //    Console.WriteLine($"Target filepath is: {filePath}");
 
-        FlaggedBibRecordModel mockBibRecord = new()
-        {
-            BibNumber = "123",
-            Action = "OUT",
-            BibTimeOfDay = "1234",
-            DayOfMonth = "12",
-            Location = "KT",
-            DataWarning = false
-        };
+    //    FlaggedBibRecordModel mockBibRecord = new()
+    //    {
+    //        BibNumber = "123",
+    //        Action = "OUT",
+    //        BibTimeOfDay = "1234",
+    //        DayOfMonth = "12",
+    //        Location = "KT",
+    //        DataWarning = false
+    //    };
 
-        WinlinkMessageModel mockMessage = new()
-        {
-            WinlinkMessageId = "ABC123DEF456",
-            ClientHostname = "TestMachine",
-            MessageDateStamp = DateTime.Now
-        };
+    //    WinlinkMessageModel mockMessage = new()
+    //    {
+    //        WinlinkMessageId = "ABC123DEF456",
+    //        ClientHostname = "TestMachine",
+    //        MessageDateStamp = DateTime.Now
+    //    };
 
-        mockMessage.BibRecords.Add(mockBibRecord);
+    //    mockMessage.BibRecords.Add(mockBibRecord);
 
-        // Act
-        var result = _fileProcessor.WriteWinlinkMessageToFile(mockMessage, filePath);
+    //    // Act
+    //    var result = _fileProcessor.WriteWinlinkMessageToFile(mockMessage, filePath);
 
-        if (File.Exists(filePath))
-        {
-            Thread.Sleep(150);
-            File.Delete(filePath);
-        }
+    //    if (File.Exists(filePath))
+    //    {
+    //        Thread.Sleep(150);
+    //        File.Delete(filePath);
+    //    }
 
-        // Assert
-        Assert.True(result);
-    }
+    //    // Assert
+    //    Assert.True(result);
+    //}
 
-    [Fact]
-    public void WriteWinlinkMessageToFile_WontWriteFileIfNoBibs()
-    {
-        // Arrange
-        var targetFolder = Environment.SpecialFolder.CommonDocuments;
-        string fileName = "testFilePath";
-        string filePath = Path.Combine(Environment.GetFolderPath(targetFolder), fileName);
-        Console.WriteLine($"Target filepath is: {filePath}");
+    // DO NOT DELETE will be reimplemented after refactoring FileProcessor
+    //[Fact]
+    //public void WriteWinlinkMessageToFile_WontWriteFileIfNoBibs()
+    //{
+    //    // Arrange
+    //    var targetFolder = Environment.SpecialFolder.CommonDocuments;
+    //    string fileName = "testFilePath";
+    //    string filePath = Path.Combine(Environment.GetFolderPath(targetFolder), fileName);
+    //    Console.WriteLine($"Target filepath is: {filePath}");
 
-        WinlinkMessageModel mockMessage = new()
-        {
-            WinlinkMessageId = "ABC123DEF456",
-            ClientHostname = "TestMachine",
-            MessageDateStamp = DateTime.Now
-        };
+    //    WinlinkMessageModel mockMessage = new()
+    //    {
+    //        WinlinkMessageId = "ABC123DEF456",
+    //        ClientHostname = "TestMachine",
+    //        MessageDateStamp = DateTime.Now
+    //    };
 
-        // Act
-        var result = _fileProcessor.WriteWinlinkMessageToFile(mockMessage, filePath);
+    //    // Act
+    //    var result = _fileProcessor.WriteWinlinkMessageToFile(mockMessage, filePath);
 
-        if (File.Exists(filePath))
-        {
-            Thread.Sleep(150);
-            File.Delete(filePath);
-        }
+    //    if (File.Exists(filePath))
+    //    {
+    //        Thread.Sleep(150);
+    //        File.Delete(filePath);
+    //    }
 
-        // Assert
-        Assert.False(result);
-    }
+    //    // Assert
+    //    Assert.False(result);
+    //}
 
     [Fact]
     public void RecordsArrayToString_ConvertsArrayToString()
