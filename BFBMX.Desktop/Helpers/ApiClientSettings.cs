cc6a6bb04eac93f29a9c49cfb36a0fcfcc91e3d1
Jon Rumsey <jonrumsey.dev@gmail.com>
2024-06-24 10:47:10 -0700

    Update api client settings, timeout, log msgs.

--- BFBMX.Desktop/Helpers/ApiClientSettings.cs
+++ BFBMX.Desktop/Helpers/ApiClientSettings.cs
@@ -1,10 +1,20 @@
-﻿namespace BFBMX.Desktop.Helpers
+﻿using System.Net.Http.Headers;
+
+namespace BFBMX.Desktop.Helpers
 {
     public class ApiClientSettings : IApiClientSettings
     {
         private readonly string _baseUri;
+        private readonly string _winlinkMessageEndpoint = "WinlinkMessage";
+        private readonly string _defaultMediaType = "application/json";
+        private readonly MediaTypeWithQualityHeaderValue _defaultMTQHJson = new("application/json");
+        private readonly TimeSpan _defaultTimeout = TimeSpan.FromSeconds(20);
 
         public string BaseUri => _baseUri;
+        public string PostWinlinkMsgUri => _winlinkMessageEndpoint;
+        public string DefaultMediaType => _defaultMediaType;
+        public MediaTypeHeaderValue DefaultMTQHJson => _defaultMTQHJson;
+        public TimeSpan DefaultTimeout => _defaultTimeout;
 
         public ApiClientSettings(string serverNameAndPort)
         {
