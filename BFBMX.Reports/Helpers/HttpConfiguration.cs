d54083279b4f5082110ca181b3c584d2e7572b06
Jon Rumsey <jonrumsey.dev@gmail.com>
2024-07-11 14:50:50 -0700

    implement reports fetching and pages

---
+++ BFBMX.Reports/Helpers/HttpConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+namespace BFBMX.Reports.Helpers
+{
+    public class HttpConfiguration
+    {
+        public string BaseAddress => "http://localhost:5150/api/v1/";
+        public string ServerInfoEndpoint => string.Concat(BaseAddress, "ServerInfo");
+        public string AllRecordsEndpoint => string.Concat(BaseAddress, "AllRecords");
+        public string DropReportEndpoint => string.Concat(BaseAddress, "DropReport");
+        public string BibNumberEndpoint => string.Concat(BaseAddress, "BibNumberReport");
+        public string AidStationEndpoint => string.Concat(BaseAddress, "AidStationReport");
+        public string StatisticsEndpoint => string.Concat(BaseAddress, "Statistics");
+        public string UserAgentHeader = "BFBMX.Reports/v1.6.0alpha";
+        public string AcceptHeader = "application/json";
+        public TimeSpan Timeout = new(0, 0, 10);
+        public CancellationTokenSource Cts => new CancellationTokenSource(Timeout);
+
+        public JsonSerializerOptions JsonOptions => new()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+    }
+}
---
+++ BFBMX.Reports/Program.cs
@@ -0,0 +1,35 @@
+using BFBMX.Reports.Components;
+using BFBMX.Reports.Helpers;
+
+namespace BFBMX.Reports
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+            builder.Services.AddHttpClient();
+            builder.Services.AddSingleton<HttpConfiguration>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
