113bfd0490f8322a4d970cd5db7be854a1f18676
Jon Rumsey <jonrumsey.dev@gmail.com>
2024-07-12 21:17:58 -0700

    Add AS Stats Model for reporting.

---
+++ BFBMX.Service/Models/AidStationStatisticsModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BFBMX.Service.Models
+{
+    public class AidStationStatisticsModel
+    {
+        public string AidStationName { get; set; } = "Unknown Aid Station";
+        public List<WinlinkMessageModel> WinlinkMessages { get; set; } = new();
+    }
+}
