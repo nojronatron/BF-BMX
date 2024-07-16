9b38a88ae54776b297bf1344b2603ca90a2e43af
Jon Rumsey <jonrumsey.dev@gmail.com>
2024-07-16 12:05:50 -0700

    Simplify bib stats model for smaller return.

--- BFBMX.Service/Models/BibRecordsStatisticsModel.cs
+++ BFBMX.Service/Models/BibRecordsStatisticsModel.cs
@@ -3,10 +3,6 @@
     public class BibRecordsStatisticsModel
     {
         public int TotalWinlinkMessagesProcessed { get; set; } = 0;
-        public int BigFootBibRecordsProcessed { get; set; } = 0;
-        public int LittleFootBibRecordsProcessed { get; set; } = 0;
-        public List<int> LittleFeetBibNumbersSeen { get; set; } = new();
-        public List<int> BigFootBibNumbersSeen { get; set; } = new();
-        public List<FlaggedBibRecordModel> UnknownBibDataItems { get; set; } = new();
+        public List<WinlinkMessageModel> AllWinlinkMessages { get; set; } = new();
     }
 }
